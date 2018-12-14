680f11bbf1d4b0683b1c01105b8bdb95a0128f1b
bglossner <benglossner@gmail.com>
2018-12-14 12:16:11 -0800

    tables

---
+++ make_tables.py
@@ -0,0 +1,18 @@
+import mysql.connector as mariadb
+
+def make_tables():
+    mariadb_connection = mariadb.connect(user='mealRoot', password='paulhatalsky', database='mealcredit')
+    cursor = mariadb_connection.cursor()
+
+    cursor.execute('create table Users (user_id INT AUTO_INCREMENT, firstname varchar(32), lastname varchar(32), username varchar(32), \
+                    phone varchar(10), email varchar(64), PRIMARY KEY (user_id), UNIQUE (username))')
+
+    cursor.execute('create table Availability (user_id INT, asking_price FLOAT NOT NULL, location varchar(64), start_time TIME, end_time TIME, \
+                    foreign key (user_id) references Users(user_id))')
+
+    cursor.execute('create table Hunger (user_id INT, max_price FLOAT NOT NULL, location varchar(64), start_time TIME, end_time TIME, \
+                    foreign key (user_id) references Users(user_id))')
+
+    mariadb_connection.close()
+
+make_tables()
