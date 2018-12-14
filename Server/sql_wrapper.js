eecd33ee0f6eeb3b8a2ade3d20323e6dfe897c55
Rupal Totale <rupaltotale>
2018-12-14 13:51:49 -0800

    2funcs

--- Server/sql_wrapper.js
+++ Server/sql_wrapper.js
@@ -98,6 +98,74 @@ module.exports = class DataAccess {
         return users;
     }
 
+    // Gets n (= limit) number of availabilities posted by a user (id = user_id)
+    async getUserAvalibiltyList(user_id, limit){
+        let availabilities = [];
+        if(limit == -1) {
+            var myQuery = `SELECT * FROM Availability WHERE location = ${user_id}`;
+        }
+        else {
+            var myQuery = `SELECT * FROM Availability WHERE location = ${user_id} LIMIT ${limit}`;
+        }
+
+        await new Promise((resolve, reject) => this._connection.query(myQuery, (err, result, fields) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                for(let element of result) {
+                    let userRet = {
+                        "location" : element.location,
+                        "start_time" : element.start_time,
+                        "end_time" : element.end_time
+                    };
+                    availabilities.push(userRet);
+                }
+                resolve(availabilities);
+            }
+        }));
+        return availabilities;
+    }
+
+    // Gives a list of people who don't have a match yet
+    async getNeedsList(location, start_time, end_time, limit, price){
+        let users = [];
+        let myQuery = `SELECT * FROM Hunger`;
+
+        if(location != "") {
+            myQuery += ` WHERE location = ${location.toLowerCase()}`;
+        }
+        if (start_time != ""){
+            myQuery += `AND start_time <= ${start_time}`;
+        }
+        if (end_time != ""){
+            myQuery += `AND end_time >= ${end_time}`;
+        }
+        if(size != -1) {
+            myQuery += ` LIMIT ${limit}`;
+        }
+
+        await new Promise((resolve, reject) => this._connection.query(myQuery, (err, result, fields) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                for(let element of result) {
+                    let userRet = {
+                        "user_id" : element.user_id,
+                        "location" : element.location,
+                        "start_time": element.start_time,
+                        "end_time": element.end_time
+                    };
+                    users.push(userRet);
+                }
+                resolve(users);
+            }
+        }));
+        
+        return users;
+    }
+
     endConnection() {
         this._connection.end();
     }
